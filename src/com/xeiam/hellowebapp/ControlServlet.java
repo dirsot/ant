bf5e908b6c1ff247405f245d6763f2b9f67efd3a
lzienkoski <lzienkowski@gmail.com>
2012-12-11 22:28:20 +0100

    start

---
+++ src/com/xeiam/hellowebapp/ControlServlet.java
@@ -0,0 +1,22 @@
+
+package com.xeiam.hellowebapp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@javax.servlet.annotation.WebServlet(urlPatterns = { "/hello" })
+public class ControlServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.setAttribute("hello_string", "Hello WebApp! 12121221");
+
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+
+    }
+}
